a54ec5940cbab3912218523673736918dd0703b8
NANAME002 <dennzerogo@gmail.com>
2025-02-17 20:34:25 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {            
+            ////Выполнить инициализацию одномерного массива с использованием конструктора без указания значений элементов и вывести на экран.
+            //Console.Write("n=");
+            //int n = int.Parse(Console.ReadLine());
+            //int[] massiv = new int[n];
+            //for (int i = 0; i < n; i++)
+            //{
+            //    Console.Write($"massiv[{i}]=");
+            //    massiv[i] = int.Parse(Console.ReadLine());
+            //}
+            //for (int i = 0; i < n; i++)
+            //{
+            //    Console.WriteLine($"massiv[{i}]={massiv[i]}");
+            //}
+            //Console.ReadLine();
+
+
+            //Console.Write("n=");
+            //int n = int.Parse(Console.ReadLine());
+            //int[] massiv = new int[n];
+            //for (int i = 0; i < n; i++)
+            //{
+            //    Console.Write($"massiv[{i}]=");
+            //    massiv[i] = int.Parse(Console.ReadLine());
+            //    Console.WriteLine($"massiv[{i}]={massiv[i]}");
+            //}
+            //Console.ReadLine();
+
+           ////Инициализировать одномерный массив с использованием конструктора , без указания размерности массива и без клавиатуры. 
+            //int[] massiv = new int[]{ 3, -3, 5, 6, -7, 8 };
+            //foreach(var element in massiv)
+            //{
+            //    Console.Write($"{element}\t");
+
+            //}
+            //Console.ReadLine();
+
+           ////Заполнить одномерный массив соответствующими индексами и вывести с использованием цикла foreach.
+            //int[] mass = new int[] {1,2,3,4,5};
+            //foreach (var element in mass)
+            //{
+            //    Console.WriteLine(mass[0]);
+            //    Console.WriteLine(mass[1]);
+            //    Console.WriteLine(mass[2]);
+            //    Console.WriteLine(mass[3]);
+            //    Console.WriteLine(mass[4]);
+            //    Console.Read();
+            //}
+
+            ////Заполнить одномерный массив случайным образом и вывести на экран с использованием цикла foreach.
+            //int[] massiv = new int[6];
+            //Random x =new Random();
+            //for(int i=0; i<massiv.Length; i++)
+            //{
+            //    //Console.WriteLine($"massiv[{i}]=");
+            //    massiv[i] = x.Next(-10,10);
+            //}    
+            //foreach(var i in massiv)
+            //{
+            //    Console.WriteLine($"{i}\t");
+            //}
+            //Console.ReadLine();
+
+        }
+    }
+    
+}
